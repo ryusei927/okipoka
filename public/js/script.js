d64648aee4b410cdfecf0b31b4bb45b238c164ec
ryusei927 <hayashiryusei0927@gmail.com>
2025-07-15 17:36:03 +0900

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
-          const section = document.querySelector('.time-based-bg');
-          const now = new Date();
-          const hour = now.getHours();
-          const minute = now.getMinutes();
+const section = document.querySelector('.time-based-bg');
+const now = new Date();
+const hour = now.getHours();
+const minute = now.getMinutes();
 
-          if (hour >= 7 && hour < 17) {
-            section.style.backgroundImage = "url('/images/asa_si-sa-png')";
-          } else if ((hour === 17) || (hour === 18 && minute < 30)) {
-            section.style.backgroundImage = "url('/images/yugata_si-sa-.png')";
-          } else {
-            section.style.backgroundImage = "url('/images/yoru_si-sa-.png')";
-          }
+if (hour >= 7 && hour < 17) {
+  section.classList.add('morning');
+} else if ((hour === 17) || (hour === 18 && minute < 30)) {
+  section.classList.add('afternoon');
+} else {
+  section.classList.add('night');
+}
