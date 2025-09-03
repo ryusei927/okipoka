b1dd1f44d6b841825cdabeeebdb0f5f208e16311
ryusei927 <hayashiryusei0927@gmail.com>
2025-09-03 09:37:48 +0900

    k

--- public/js/header.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navMenu");
-
-hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
-    hamburger.classList.toggle("active");
-});
